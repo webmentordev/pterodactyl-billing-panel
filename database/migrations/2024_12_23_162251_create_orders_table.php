c46c23ac2674df746fd8cb5762d4cb2c8b72df93
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2025-01-12 13:15:01 +0500

    Fixed Type

--- database/migrations/2024_12_23_162251_create_orders_table.php
+++ database/migrations/2024_12_23_162251_create_orders_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->text('checkout_url')->nullable();
             $table->timestamp('expire_at')->nullable();
             $table->timestamp('refund_at')->nullable();
-            $table->text('total_payments')->default(0);
+            $table->bigInteger('total_payments')->default(0);
             $table->decimal('price', 10, 2);
             $table->boolean('has_emailed')->default(false);
             $table->boolean('is_active')->default(false);
