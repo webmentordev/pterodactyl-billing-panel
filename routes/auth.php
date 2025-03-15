34b7c0a11d898d2bc03ce7eed686aacda2abe764
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2025-03-15 08:17:15 +0500

    Protection, Throttle

--- routes/auth.php
+++ routes/auth.php
@@ -12,10 +12,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    Route::get('/user/account/create/new', [RegisteredUserController::class, 'create'])
         ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('/user/account/create/new', [RegisteredUserController::class, 'store'])
+        ->middleware(['throttle:3,60']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
@@ -44,7 +45,7 @@ Route::middleware('auth')->group(function () {
         ->name('verification.verify');
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
+        ->middleware('throttle:2,20')
         ->name('verification.send');
 
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
