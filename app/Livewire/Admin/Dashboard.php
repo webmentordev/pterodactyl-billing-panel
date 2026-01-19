47104e10ad758d8fb70675157c2195f212a67b39
webmentordev <ahmertahir99@gmail.com>
2026-01-19 16:10:07 +0000

    Dashboard

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\User;
+use App\Models\Trial;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 
@@ -11,6 +13,10 @@ class Dashboard extends Component
     #[Layout('layouts.livewire.admin')]
     public function render()
     {
-        return view('livewire.admin.dashboard');
+        return view('livewire.admin.dashboard', [
+            "users" => User::count(),
+            "trials" => Trial::count(),
+            "pending_trials" => Trial::where('status', 'pending')->count(),
+        ]);
     }
 }
--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,3 +1,16 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-</div>
+<section class="w-full h-full">
+    <div class="max-w-xl m-auto w-full grid grid-cols-2 gap-3">
+        <div class="p-5 bg-dark rounded-lg shadow-sm">
+            <h3 class="text-3xl mb-3 text-white">Registered Users</h3>
+            <span class="text-gray-300">Total: <strong>{{ $users }}</strong></span>
+        </div>
+        <div class="p-5 bg-dark rounded-lg shadow-sm">
+            <h3 class="text-3xl mb-3 text-white">Trial Requests</h3>
+            <span class="text-gray-300">Total: <strong>{{ $trials }}</strong></span>
+        </div>
+        <div class="p-5 bg-dark rounded-lg shadow-sm">
+            <h3 class="text-3xl mb-3 text-rust">Pending Trials</h3>
+            <span class="text-gray-300">Total: <strong>{{ $pending_trials }}</strong></span>
+        </div>
+    </div>
+</section>
